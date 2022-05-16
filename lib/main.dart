4292ee5ac5aa9af0ff664a3204b218f66dad4cd2
Daniyal Saeed <daniyal.saeed7829@gmail.com>
2022-05-16 10:17:36 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_login/app.dart';
+import 'package:user_repository/user_repository.dart';
+
+void main() {
+  runApp(App(
+    authenticationRepository: AuthenticationRepository(),
+    userRepository: UserRepository(),
+  ));
+}
